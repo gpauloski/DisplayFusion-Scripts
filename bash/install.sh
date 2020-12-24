a78790a35a5150f82c50d523970a0100e1d1e57b
Greg Pauloski <gpauloski@yahoo.com>
2020-12-24 13:36:20 -0600

    Vim SuperTab and OSC italic support

--- bash/install.sh
+++ bash/install.sh
@@ -16,11 +16,13 @@ ln -s $PWD/bash_aliases ~/.bash_aliases
 # VIMRC
 #
 
+if [ ! -d ~/.vim/ ]; then
+    mkdir ~/.vim/
+fi
+
 if [ -f ~/.vim/vimrc ]; then
     mv ~/.vim/vimrc ~/.vim/vimrc.old
 fi
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s $PWD/vimrc ~/.vim/vimrc
 
